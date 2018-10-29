59207b5c1dcd65899bc1f40d4e9922eeece82abc
mejikan <lawrence.raphael.castro.yong@gmail.com>
2018-10-29 12:48:44 -0400

    Main commit - Vue stuff added

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+	<v-app>
+		<v-toolbar app>
+			<v-toolbar-title class="headline text-uppercase">
+				<span>Liquor Insights </span>
+				<span class="font-weight-light">A 336 PROJECT</span>
+			</v-toolbar-title>
+			<v-spacer></v-spacer>
+			<v-btn
+				flat
+				target="_blank"
+			>
+				<span class="mr-2">Drinker</span>
+			</v-btn>
+			<v-btn
+				flat
+				target="_blank"
+			>
+				<span class="mr-2">Bar</span>
+			</v-btn>
+			<v-btn
+				flat
+				target="_blank"
+			>
+				<span class="mr-2">Beer</span>
+			</v-btn>
+		</v-toolbar>
+
+		<v-content>
+			<HelloWorld/>
+		</v-content>
+	</v-app>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+
+export default {
+	name: "App",
+	components: {
+		HelloWorld,
+	},
+	data() {
+		return {
+			//
+		};
+	},
+};
+</script>
