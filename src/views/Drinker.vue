7cc945efc7cf4d3b05225bdf6903da020a1ec2f3
mejikan <lawrence.raphael.castro.yong@gmail.com>
2018-10-30 15:03:46 -0400

    can view drinkers on drinker page

--- src/views/Drinker.vue
+++ src/views/Drinker.vue
@@ -9,18 +9,17 @@
 						</v-card-title>
 						<v-card-text>
 							<v-data-table
-								:headers="headers"
-								:items="desserts"
+								:headers="tableHeaders"
+								:items="tableRows"
 								hide-actions
 								class="elevation-1"
 							>
 								<template slot="items" slot-scope="props">
 									<td>{{ props.item.name }}</td>
-									<td class="text-xs-right">{{ props.item.calories }}</td>
-									<td class="text-xs-right">{{ props.item.fat }}</td>
-									<td class="text-xs-right">{{ props.item.carbs }}</td>
-									<td class="text-xs-right">{{ props.item.protein }}</td>
-									<td class="text-xs-right">{{ props.item.iron }}</td>
+									<td>{{ props.item.city }}</td>
+									<td>{{ props.item.state }}</td>
+									<td>{{ props.item.phone }}</td>
+									<td>{{ props.item.addr }}</td>
 								</template>
 							</v-data-table>
 							<Chart/>
@@ -35,6 +34,7 @@
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 
+import axios from "axios";
 import Chart from "@/components/Chart.vue";
 
 @Component({
@@ -45,115 +45,48 @@ import Chart from "@/components/Chart.vue";
 
 export default class Drinker extends Vue {
 
-	private headers: any = [
+	private tableHeaders: any[] = [
 		{
-			text: "Dessert (100g serving)",
+			text: "Name",
 			align: "left",
 			sortable: false,
 			value: "name",
 		},
-		{ text: "Calories", value: "calories" },
-		{ text: "Fat (g)", value: "fat" },
-		{ text: "Carbs (g)", value: "carbs" },
-		{ text: "Protein (g)", value: "protein" },
-		{ text: "Iron (%)", value: "iron" },
+		{ text: "City", value: "city" },
+		{ text: "State", value: "state" },
+		{ text: "Phone", value: "phone" },
+		{ text: "Address", value: "addr" },
 	];
 
-	private desserts: any = [
-		{
-		value: false,
-		name: "Frozen Yogurt",
-		calories: 159,
-		fat: 6.0,
-		carbs: 24,
-		protein: 4.0,
-		iron: "1%",
-		},
-		{
-		value: false,
-		name: "Ice cream sandwich",
-		calories: 237,
-		fat: 9.0,
-		carbs: 37,
-		protein: 4.3,
-		iron: "1%",
-		},
-		{
-		value: false,
-		name: "Eclair",
-		calories: 262,
-		fat: 16.0,
-		carbs: 23,
-		protein: 6.0,
-		iron: "7%",
-		},
-		{
-		value: false,
-		name: "Cupcake",
-		calories: 305,
-		fat: 3.7,
-		carbs: 67,
-		protein: 4.3,
-		iron: "8%",
-		},
-		{
-		value: false,
-		name: "Gingerbread",
-		calories: 356,
-		fat: 16.0,
-		carbs: 49,
-		protein: 3.9,
-		iron: "16%",
-		},
-		{
-		value: false,
-		name: "Jelly bean",
-		calories: 375,
-		fat: 0.0,
-		carbs: 94,
-		protein: 0.0,
-		iron: "0%",
-		},
-		{
-		value: false,
-		name: "Lollipop",
-		calories: 392,
-		fat: 0.2,
-		carbs: 98,
-		protein: 0,
-		iron: "2%",
-		},
-		{
-		value: false,
-		name: "Honeycomb",
-		calories: 408,
-		fat: 3.2,
-		carbs: 87,
-		protein: 6.5,
-		iron: "45%",
-		},
-		{
-		value: false,
-		name: "Donut",
-		calories: 452,
-		fat: 25.0,
-		carbs: 51,
-		protein: 4.9,
-		iron: "22%",
-		},
-		{
-		value: false,
-		name: "KitKat",
-		calories: 518,
-		fat: 26.0,
-		carbs: 65,
-		protein: 7,
-		iron: "6%",
-		},
-	];
+	private tableRows: any[] = [];
+
+	private async setTableRows(): Promise<void> {
+		let fullURL: string = "http://localhost:8081/something/something?q=";
+		fullURL += encodeURIComponent("SELECT * FROM BarBeerDrinker.drinkers LIMIT 20");
+		const response = await axios.get(fullURL);
+		let results: any[] = [];
+		if (response.status === 200) {
+			const rows: any[] = response.data as any[];
+			for (let row of rows) {
+				const result = {
+					value: false,
+					name: row.name,
+					city: row.city,
+					state: row.state,
+					phone: row.phone,
+					addr: row.addr,
+				}
+				results.push(result); 
+				console.log(result);
+			}
+			this.tableRows = results;
+		} else {
+			console.error("Failed to get data");
+		}
+	}
 
 	private mounted() {
-		//
+		this.setTableRows();
 	}
 }
 </script>
