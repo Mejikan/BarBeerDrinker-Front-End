9d4e813aee4abac8e5bf3a63628bc87469e02738
Lawrence <lawrence.raphael.castro.yong@gmail.com>
2018-11-15 00:29:13 -0500

    added stock photos

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,18 @@
 <template>
 	<div class="home">
+		<v-container fluid fill-height class="pa-0 ma-0">
+			<v-carousel
+				hide-controls
+				hide-delimiters
+				height="100%"
+			>
+				<v-carousel-item
+					v-for="(item,i) in images"
+					:key="i"
+					:src="item.src"	
+				></v-carousel-item>
+			</v-carousel>
+		</v-container>
 	</div>
 </template>
 
@@ -10,5 +23,14 @@ import { Component, Vue } from "vue-property-decorator";
 	components: {
 	},
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+	private images: any[] = [
+		{ src: require("@/assets/stock/beer-machine-alcohol-brewery-159291.jpeg") },
+		{ src: require("@/assets/stock/bread-food-restaurant-people.jpg") },
+		{ src: require("@/assets/stock/jause-eat-delicious-food-56007.jpeg") },
+		{ src: require("@/assets/stock/pexels-photo-1128975.jpeg") },
+		{ src: require("@/assets/stock/pexels-photo-1267359.jpeg") },
+		{ src: require("@/assets/stock/pexels-photo-1283219.jpeg") },
+	];
+}
 </script>
